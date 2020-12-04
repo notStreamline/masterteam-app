0e634fbade926a213ff54805bca7f7289f86cdd8
Maria Isabel Lopez <mrlpz1515@gmail.com>
2020-12-03 23:44:45 -0500

    Const, dynamic menu, typesctript, reset.css

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <li v-if="isVisible" class="menu-item rounded">
+      <router-link
+        exact-active-class="text-success"
+        :to="{name: route.name}"
+        class="nav-link"
+      >{{name}}</router-link>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   * TODO: add the ts tag in the script. Correct the errors
+   */
+  name: 'menu-item',
+  props: {
+    route: Object,
+  },
+  computed: {
+    isVisible() {
+      return this.route?.meta?.visible !== false;
+    },
+    name() {
+      return this.route.name
+        .toLowerCase()
+        .split('-')
+        .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+        .join(' ');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+/* The CSS goes here */
+.nav-link {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="navbar-area">
+    <nav class="navbar navbar-area">
+      <div class="row container nav-container navbar-expand-lg">
+        <div class="logo">
+          LOGO
+        </div>
+        <ul class="navbar-nav">
+          <menu-item v-for="(r,i) in routes" :key="i" :route="r"/>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   * Agregar Mobile navbar
+   */
+  name: 'the-navbar',
+  components: {
+    MenuItem: () => import(/* webpackChunkName: "menu-item" */ './MenuItem.vue'),
+  },
+  computed: {
+    routes() {
+      return this.$router.options.routes;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+/* Your CSS goes here */
+.navbar-area{
+  padding: 0;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 9999;
+  -webkit-transition: all .4s;
+  -o-transition: all .4s;
+  transition: all .4s;
+
+  .navbar{
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: justify;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+    padding: .5rem 1rem;
+  }
+
+  .nav-container {
+    background-color: transparent;
+    padding: 22px 0px;
+    -webkit-transition: all .4s;
+    -o-transition: all .4s;
+    transition: all .4s;
+  }
+
+  .navbar-nav {
+    width: 100%;
+    display: flex;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+    .menu-item {
+      margin-right: 20px;
+    }
+}
+
+.logo{
+  padding: 10px;
+}
+
+@media only screen and (max-width: 1399px) and (min-width: 1200px){
+  .navbar-area .nav-container {
+    max-width: 95%;
+  }
+}
+</style>
